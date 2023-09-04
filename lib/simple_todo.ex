060690b07db68720444ba842247beac3f9e51114
Michael Edoror <medoror@gmail.com>
2023-09-04 18:57:12 -0500

    Update and delete

--- lib/simple_todo.ex
+++ lib/simple_todo.ex
@@ -29,6 +29,22 @@ defmodule TodoList do
   def entries(todo_list, date) do
     todo_list.entries |> Map.values() |> Enum.filter(fn entry -> entry.date == date end)
   end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error -> todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
+  end
 end
 
 
